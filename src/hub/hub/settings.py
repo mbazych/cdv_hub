77fca1ae67bb95b596e8d117d56aff686fd6b152
mbazych <44398475+mbazych@users.noreply.github.com>
2020-09-16 17:13:12 +0200

    'postgres'

--- src/hub/hub/settings.py
+++ src/hub/hub/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'hub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'cdv_hub',
     }
 }
