6882bfd90928bc60b1ce366014fa6503a97bb00e
mbazych <44398475+mbazych@users.noreply.github.com>
2020-09-16 17:27:02 +0200

    fix in ip address in settings.py

--- src/hub/hub/settings.py
+++ src/hub/hub/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'cdv_hub',
-        'HOST': '10.253.1.3 ',
+        'HOST': '10.253.1.3',
         'PORT': '5432'
     }
 }
